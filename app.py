0ddb56bf7053181d81985b6ea7e6390245b4dbd9
Pranesh <pranesh@Praneshs-MacBook-Air.local>
2026-06-08 11:42:17 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "<h1>Hello DevOps World</h1>"
+
+print("File loaded")
+
+if __name__ == "__main__":
+    print("Starting Flask...")
+    app.run(host="0.0.0.0", port=8080)
